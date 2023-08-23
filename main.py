ff5a79b36006aebe49e901c37a657a1039c09c9c
bennyrubanov <106097466+bennyrubanov@users.noreply.github.com>
2023-08-23 10:49:48 +0000

    Textblob implementation (without multi-language support)

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 ###### import libraries and api keys ######
 
-#v2 github test
-#v2 test again
-
-import os, requests, json, replicate
-from flask import Flask, request, jsonify, render_template
+import os, requests
+from flask import Flask, request, jsonify
+from textblob import TextBlob
 
 #os.environ["REPLICATE_API_TOKEN"] = REPLICATE_API_TOKEN
 
@@ -26,34 +24,32 @@ def result():
     negative_headlines_ratios = []  # Store the ratios in a list
 
     # Iterate through the results of the asynchronous generator
-    for result in negativeHeadlinesRatioCalc(articles):
-      negative_headlines_ratio = result['ratio_bad_to_good']
-      negative_headlines_ratios.append(negative_headlines_ratio)
+    for result in sentimentAnalysis(articles):
+      average_sentiment = result['average_sentiment']
 
-    # Calculate the overall negative headlines ratio
-    overall_negative_headlines_ratio = sum(negative_headlines_ratios) / len(
-      negative_headlines_ratios)
-    normalized_negative_headlines_ratio = overall_negative_headlines_ratio * 10  # To put on a scale of 0-10
+    # Normalize the average sentiment calculation
+    normalized_average_sentiment = ((average_sentiment + 1) / 2) * 10  # To move from range -1 to 1, to 0 to 1, and then 0 to 10
+    print(f"normalized average sentiment: {normalized_average_sentiment}")
     overall_tolerance_score = 10 * (
-      10 - normalized_negative_headlines_ratio) * (tolerance_from_user / 10)
+      10 - normalized_average_sentiment) * (tolerance_from_user / 10)
 
     message = ""
     if tolerance_from_user == 0:
       message += "Your tolerance is quite low... news generally may not be your thing, friend. Let's check the headlines though...\n"
-      if overall_negative_headlines_ratio == 0:
+      if normalized_average_sentiment == 0:
         message += "\n Hey, looks like you're in luck! All articles are looking either positive or neutral. You're probably safe to read the news today, but given you're fragile, proceed with caution 👀"
       else:
         message += "\n\n I'm seeing some negative articles. Given you're fragile, I don't recommend reading the news today 💗"
     elif tolerance_from_user == 10:
       message += "You can tolerate anything! Read the news!\n"
-      if overall_negative_headlines_ratio == 0:
+      if normalized_average_sentiment == 0:
         message += "\n Oh, and also, the news is all positive anyways 🙂"
-      elif overall_negative_headlines_ratio == 1:
-        message += "\n That being said... the news is a pretty poopy today. Proceed with caution..."
+      elif normalized_average_sentiment == 10:
+        message += "\n That being said... the news is pretty poopy today. Proceed with caution..."
     else:
-      if overall_negative_headlines_ratio == 0:
+      if normalized_average_sentiment == 0:
         message += "Hey, looks like today, all articles are looking pretty positive! You're probably safe to read the news today.\n"
-      elif overall_negative_headlines_ratio == 10:
+      elif normalized_average_sentiment == 10:
         message += "All articles are negative. Proceed with caution!\n"
       else:
         if overall_tolerance_score >= 75:
@@ -65,11 +61,11 @@ def result():
         elif overall_tolerance_score < 50 and overall_tolerance_score > 25:
           message += "Consider not reading the news. Doesn't look too fun."
         elif overall_tolerance_score <= 25:
-          message += "Best to avoid reading the news today. It's quite negative."
+          message += "Best to avoid reading the news today. It looks to be too negative for you."
     # Return the articles and sentiment analysis as JSON
     response_data = {
       'articles': articles,
-      'ratio_bad_to_good': overall_negative_headlines_ratio,
+      'ratio_bad_to_good': normalized_average_sentiment,
       'tolerance_score': overall_tolerance_score,
       'message': message  # Add the message key to the response_data dictionary
     }
@@ -101,71 +97,39 @@ def fetch_news_articles(country):
   url = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={newsKey}&pageSize={pageSize}"
   result = requests.get(url)
   data = result.json()
-  print(data.get('articles', []))
+  #print(data.get('articles', []))
   return data.get('articles', [])
 
 
-def negativeHeadlinesRatioCalc(articles):
+def sentimentAnalysis(articles):
 
-  REPLICATE_API_TOKEN = os.environ['REPLICATE_API_TOKEN']
+  #REPLICATE_API_TOKEN = os.environ['REPLICATE_API_TOKEN']
 
   ###### running llama2 from replicate ######
 
   numArticles = 0
-  negativeArticles = 0
-  neutralArticles = 0
-  positiveArticles = 0
+  article_sentiment_polarity = 0
 
-  for article in articles[:5]:
-    prompt = (
-      f"""Is the following headline positive, negative, or neutral? {article["title"]}"""
-    )
-    output = replicate.run(
-      "a16z-infra/llama-2-13b-chat:d5da4236b006f967ceb7da037be9cfc3924b20d21fed88e1e94f19d56e2d3111",
-      input={
-        "prompt": prompt,
-        "system_prompt":
-        "You can only respond with one word: 'Positive', 'Negative', or 'Neutral'.",
-        "max_new_tokens": 5
-      })
-
-    outputSTRING = ""
-
-    for item in output:
-      outputSTRING += item
-
-    if "Negative" in outputSTRING:
-      negativeArticles += 1
-      article["sentiment"] = "Negative"
-    elif "Neutral" in outputSTRING:
-      neutralArticles += 1
-      article["sentiment"] = "Neutral"
-    elif "Positive" in outputSTRING:
-      positiveArticles += 1
-      article["sentiment"] = "Positive"
-
+  for article in articles:
+    prompt = TextBlob(f"""{article["title"]}""")
+    article_sentiment_polarity += float(prompt.sentiment.polarity)
     numArticles += 1
 
-    print(f"outputString: {outputSTRING}")
-    print(f"number of negative articles: {negativeArticles}")
-    print(f"number of neutral articles: {neutralArticles}")
-    print(f"number of positive articles: {positiveArticles}")
+    print(f"""{article["title"]}""")
+    print(f"""{article_sentiment_polarity}""")
 
-    ratio_bad_to_good_headlines = negativeArticles / numArticles
-
+    average_sentiment = article_sentiment_polarity / numArticles
+    
     # Yield the result for each article one by one
     yield {
       'article': article,
-      'ratio_bad_to_good': ratio_bad_to_good_headlines,
+      'average_sentiment': average_sentiment,
     }
 
-  ratio_bad_to_good_headlines = negativeArticles / numArticles
+  average_sentiment = article_sentiment_polarity / numArticles
+  print(f"average sentiment of articles: {average_sentiment}")
 
-  print(f"number of negative articles: {negativeArticles}")
-  print(f"number of neutral articles: {neutralArticles}")
-  print(f"number of positive articles: {positiveArticles}")
-  print(f"ratio of bad to good headlines: {ratio_bad_to_good_headlines}")
-  return ratio_bad_to_good_headlines, articles
+  return average_sentiment, articles
 
 
 if __name__ == '__main__':
